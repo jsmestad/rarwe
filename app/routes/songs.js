b7a847c5c6a0fc3064de1e341af3475133d97522
Justin Smestad <justin.smestad@gmail.com>
2015-04-20 15:37:07 -0600

    End of Chapter 4

--- app/routes/songs.js
+++ app/routes/songs.js
@@ -1,4 +1,41 @@
 import Ember from 'ember';
 
+var Song = Ember.Object.extend({
+  title: '',
+  rating: 0,
+  band: ''
+});
+
+var SongCollection = Ember.ArrayProxy.extend(Ember.SortableMixin, {
+  sortProperties: ['rating'],
+  sortAscending: false,
+  content: []
+});
+
+var songs = SongCollection.create();
+
+var blackDog = Song.create({
+  title: 'Black Dog',
+  band: 'Led Zeppelin',
+  rating: 3
+});
+
+var yellowLedbetter = Song.create({
+  title: 'Yellow Ledbetter',
+  band: 'Pearl Jam',
+  rating: 4
+});
+
+var pretender = Song.create({
+  title: 'The Pretender',
+  band: 'Foo Fighters',
+  rating: 2
+});
+
+songs.pushObjects([blackDog, yellowLedbetter, pretender]);
+
 export default Ember.Route.extend({
+  model: function() {
+    return songs;
+  }
 });

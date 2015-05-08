1068c35c1463ecfe95e83d8601f1bfa7e5902226
Justin Smestad <justin.smestad@gmail.com>
2015-05-08 15:35:29 -0600

    end of Chapter 11

--- tests/unit/components/star-rating-test.js
+++ tests/unit/components/star-rating-test.js
@@ -1,21 +1,42 @@
+import Ember from 'ember';
 import {
   moduleForComponent,
   test
 } from 'ember-qunit';
 
-moduleForComponent('star-rating', {
-  // Specify the other units that are required for this test
-  // needs: ['component:foo', 'helper:bar']
-});
+moduleForComponent('star-rating', 'Unit - Star rating component');
 
-test('it renders', function(assert) {
-  assert.expect(2);
+test('Renders the full and empty stars correctly', function(assert) {
+  assert.expect(4);
 
-  // Creates the component instance
   var component = this.subject();
-  assert.equal(component._state, 'preRender');
+  Ember.run(function() {
+    component.setProperties({
+      rating: 4,
+      maxRating: 5
+    });
+  });
 
-  // Renders the component to the page
-  this.render();
-  assert.equal(component._state, 'inDOM');
+  assert.equal(this.$().find('.glyphicon-star').length, 4, "The right amount of full starts is rendered");
+  assert.equal(this.$().find('.glyphicon-star-empty').length, 1, "The right amount of empty stars is rendered");
+
+  Ember.run(function() {
+    component.set('maxRating', 10);
+  });
+
+
+  assert.equal(this.$().find('.glyphicon-star').length, 4, "The right amount of full starts is rendered after changing maxRating");
+  assert.equal(this.$().find('.glyphicon-star-empty').length, 6, "The right amount of empty stars is rendered after changing maxRating");
 });
+
+// test('it renders', function(assert) {
+  // assert.expect(2);
+
+  // // Creates the component instance
+  // var component = this.subject();
+  // assert.equal(component._state, 'preRender');
+
+  // // Renders the component to the page
+  // this.render();
+  // assert.equal(component._state, 'inDOM');
+// });

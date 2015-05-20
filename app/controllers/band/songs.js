fff44974d3e4c33fe7db2cc1ad7f710dc0639955
Justin Smestad <justin.smestad@gmail.com>
2015-05-20 12:55:38 -0600

    end of Chapter 12

--- app/controllers/band/songs.js
+++ app/controllers/band/songs.js
@@ -1,8 +1,33 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  queryParams: {
+    sortBy: 'sort',
+    searchTerm: 's'
+  },
+
   title: '',
   songCreationStarted: false,
+
+  sortBy: 'ratingDesc',
+  sortProperties: function() {
+    var options = {
+      "ratingDesc": "rating:desc,title:asc",
+      "ratingAsc": "rating:asc,title:asc",
+      "titleDesc": "title:desc",
+      "titleAsc": "title:asc"
+    };
+    return options[this.get('sortBy')].split(',');
+  }.property('sortBy'),
+  sortedSongs: Ember.computed.sort('matchingSongs', 'sortProperties'),
+
+  searchTerm: '',
+  matchingSongs: function() {
+    var searchTerm = this.get('searchTerm').toLowerCase();
+    return this.get('model').filter(function(song) {
+      return song.get('title').toLowerCase().indexOf(searchTerm) !== -1;
+    });
+  }.property('model.@each.title', 'searchTerm'),
 
   isAddButtonDisabled: function() {
     return Ember.isEmpty(this.get('title'));
@@ -19,6 +44,9 @@ export default Ember.Controller.extend({
   actions: {
     enableSongCreation: function() {
       this.set('songCreationStarted', true);
+    },
+    setSorting: function(option) {
+      this.set('sortBy', option);
     }
   }
 });
--- tests/acceptance/bands-test.js
+++ tests/acceptance/bands-test.js
@@ -85,3 +85,74 @@ test('Create a new song in two steps', function(assert) {
     assert.equal(find('.songs .song:contains("Killer Cars")').length, 1, "Creates the song and displays it in the list");
   });
 });
+
+test('Sort songs in various ways', function(assert) {
+  server = new Pretender(function() {
+    httpStubs.stubBands(this, {
+      bands: [
+        { id: 1, name: 'Them Crooked Vultures', songs: [1, 2, 3, 4] }
+      ],
+      songs: [
+        { id: 1, title: 'Elephants', rating: 5 },
+        { id: 2, title: 'New Fang', rating: 4 },
+        { id: 3, title: 'Mind Eraser, No Chaser', rating: 4 },
+        { id: 4, title: 'Spinning in Daffodils', rating: 5 }
+      ]
+    });
+  });
+
+  selectBand('Them Crooked Vultures');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/bands/1/songs');
+    assertTrimmedText(assert, '.song:first', 'Elephants', "The first song is the highest ranked, first in the alphabet");
+    assertTrimmedText(assert, '.song:last', 'New Fang', "The last song is the lowest ranked, last in the alphabet");
+  });
+
+  click('button.sort-title-desc');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/bands/1/songs?sort=titleDesc');
+    assertTrimmedText(assert, '.song:first', 'Spinning in Daffodils', "The first song is the one that is the last in the alphabet");
+    assertTrimmedText(assert, '.song:last', 'Elephants', "The last song is the one that is the first in the alphabet");
+  });
+
+  click('button.sort-rating-asc');
+
+  andThen(function() {
+    assertTrimmedText(assert, '.song:first', 'Mind Eraser, No Chaser', "The first song is the lowest ranked, first in the alphabet");
+    assertTrimmedText(assert, '.song:last', 'Spinning in Daffodils', "The last song is the highest ranked, last in the alphabet");
+  });
+});
+
+test('Search songs', function(assert) {
+  server = new Pretender(function() {
+    httpStubs.stubBands(this, {
+      bands: [
+        { id: 1, name: 'Them Crooked Vultures', songs: [1,2,3,4,5] }
+      ],
+      songs: [
+        { id: 1, title: 'Elephants', rating: 5 },
+        { id: 2, title: 'New Fang', rating: 4 },
+        { id: 3, title: 'Mind Eraser, No Chaser', rating: 4 },
+        { id: 4, title: 'Spinning in Daffodils', rating: 5 },
+        { id: 5, title: 'No One Loves Me & Neither Do I', rating: 3}
+      ]
+    });
+  });
+
+  visit('/bands/1/songs');
+
+  fillIn('.search-field', 'no');
+
+  andThen(function() {
+    assertLength(assert, '.song', 2, "The songs matching the search term are displayed");
+  });
+
+  click('button.sort-title-desc');
+
+  andThen(function() {
+    assertTrimmedText(assert, '.song:first', 'No One Loves Me & Neither Do I');
+    assertTrimmedText(assert, '.song:last', 'Mind Eraser, No Chaser');
+  });
+});
